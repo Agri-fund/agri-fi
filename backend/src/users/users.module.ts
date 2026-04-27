1c9328f9665d93a82f86b621d38ab6a1545288bc
kryputh <uthaiminlawal@gmail.com>
2026-04-27 21:09:04 +0100

    fix: update entity imports and remove stale duplicates

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -3,8 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from '../auth/entities/user.entity'; // Corrected Path
-import { TradeDeal } from './entities/trade-deal.entity';
-import { Investment } from './entities/investment.entity';
+import { TradeDeal } from '../trade-deals/entities/trade-deal.entity';
+import { Investment } from '../investments/entities/investment.entity';
 import { ShipmentMilestone } from '../shipments/entities/shipment-milestone.entity';
 import { PaymentDistribution } from '../escrow/entities/payment-distribution.entity';
 
